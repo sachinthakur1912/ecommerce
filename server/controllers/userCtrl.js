de28ea979c4995f337c6bb959e6135677d694d01
Sachin Thakur <sachinthakur@Sachins-MacBook-Air.local>
2024-05-10 23:52:29 +0530

    auth

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -113,7 +113,7 @@ const userCtrl = {
         console.error(error)
      }  
   },
-  getUser:async(req,res) => {
+  getUsers:async(req,res) => {
     try{
         const user = await Users.findById(req.user.id).select('-password')
         if(!user) return res.json({msg:"User not found"})
--- server/routes/useRoute.js
+++ server/routes/useRoute.js
@@ -7,5 +7,5 @@ router.post('/register',userCtrl.register)
 router.post('/refresh_token',userCtrl.refreshToken)
 router.post('/login',userCtrl.login)
 router.get('/logout',userCtrl.logout)
-router.get('/info',auth,userCtrl.getUser)
+router.get('/info',auth,userCtrl.getUsers)
 module.exports = router;
