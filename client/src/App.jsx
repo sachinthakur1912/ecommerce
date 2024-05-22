4fe77f80c66ec28c2cc9179ac6ccbeac85df8b7f
sachinthakur1912 <thakursachin1912@gmail.com>
2024-05-23 00:01:29 +0530

    Navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/headers/Header'
+import Pages from './components/mainpages/Pages'
+
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Header/>
+      <Pages/>
+    </div>
+  )
+}
---
+++ client/src/components/headers/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { IoMdMenu } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
+import { IoMdCart } from "react-icons/io";
+import { Link } from 'react-router-dom';
+// import './header.css'
+export default function Header() {
+  return (
+    <header>
+      <div className='menu'>
+      <IoMdMenu width={30}/>
+      </div>
+      <div className='logo'>
+        <h1>
+          <Link to='/'>30DC Shop</Link>
+        </h1>
+      </div>
+      <ul>
+        <li><Link to='/'>Products</Link></li>
+        <li><Link to='/login'>Login or Register</Link></li>
+        <li><IoMdClose size={30} className='menu' /></li>
+      </ul>
+      <div className='cart-icon'>
+        <span>
+          0
+        </span>
+        <Link><IoMdCart size={30} /></Link>
+      </div>
+      
+    </header>
+  )
+}
